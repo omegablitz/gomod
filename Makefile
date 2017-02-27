d03d5318611e8bf9149bcc3298acb88f31b7c745
Harshal Sheth <hsheth2@gmail.com>
2017-02-26 22:43:55 -0500

    finished spoofing?

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ all: gomod.o
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 
 gomod.o: gomod.go;
-	go build -buildmode=c-archive gomod.go
+	go build -buildmode=c-archive --ldflags '-extldflags "-static"' gomod.go
 clean:
 	-rm -f gomod.h
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- runtime.c
+++ runtime.c
@@ -9,3 +9,33 @@ void * malloc(unsigned long size) {
 void free(void *addr) {
 	vfree(addr);
 }
+
+size_t fwrite(void* ptr, size_t size, size_t nmemb, void* stream){return 0;}
+int fputc(int chr, void *stream){
+	return 0;
+}
+
+void __vfprintf_chk(void){}
+void __fprintf_chk(void){}
+
+void __errno_location(void){}
+void pthread_cond_wait(void){}
+void pthread_mutex_lock(void){}
+void sigismember(void){}
+void stderr(void){}
+void pthread_mutex_unlock(void){}
+void sigaction(void){}
+void sigfillset(void){}
+void pthread_sigmask(void){}
+void setenv(void){}
+void pthread_cond_broadcast(void){}
+void nanosleep(void){}
+void mmap(void){}
+void sigaddset(void){}
+void pthread_attr_init(void){}
+void strerror(void){}
+void sigemptyset(void){}
+void pthread_create(void){}
+void pthread_attr_getstacksize(void){}
+void pthread_attr_destroy(void){}
+void unsetenv(void){}
